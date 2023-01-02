63ef2c50be7b311c2958fc85eb71dff87ca5f1b8
Keith Carangelo <mail@kcaran.com>
2023-01-02 08:33:22 -0500

    Finished Day 23!

--- day23.pl
+++ day23.pl
@@ -26,7 +26,7 @@ my %energy = (
      next unless ((my $pod = $self->{ hall }[$idx]) =~ /[A-D]/);
      # Check if we can move in
      my $room = ord( $pod ) - 65;
-     next unless (!$self->{ rooms }[$room] || $self->{ rooms }[$room] eq $pod);
+     next unless (!$self->{ rooms }[$room] || $self->{ rooms }[$room] =~ /^$pod+$/);
      if ($idx <= ($room * 2 + 2)) {
        $hall = $idx + 1;
        while (($self->{ hall }[$hall] ne lc($pod)) && ($self->{ hall }[$hall] !~ /[A-D]/)) {
@@ -44,7 +44,7 @@ my %energy = (
 
      my $next = Storable::dclone( $self );
      $next->{ energy } += abs($hall - $idx) * $energy{ $pod };
-     $next->{ energy } += (2 - length( $self->{ rooms }[$room] )) * $energy{ $pod };
+     $next->{ energy } += ($self->{ spots } - length( $self->{ rooms }[$room] )) * $energy{ $pod };
      $next->{ rooms }[$room] .= $pod;
      $next->{ hall }[$idx] = '';
      push @moves, $next;
@@ -63,8 +63,7 @@ my %energy = (
    my $pod = substr( $self->{ rooms }[$idx], -1, 1, '' );
 
    my @moves;
-   my $out = $energy{ $pod };
-   $out += $energy{ $pod } unless ($self->{ rooms }[$idx]);
+   my $out = ($self->{ spots } - length( $self->{ rooms }[$idx] )) * $energy{ $pod };
    my $left = 2 * ($idx + 1) - 1;
    while ($left >= $self->{ left }{ $pod }) {
      $out += $energy{ $pod };
@@ -76,12 +75,12 @@ my %energy = (
        # Check if we can move in
        my $room = ord($self->{ hall }[$left]) - 97;
        if ($self->{ hall }[$left] eq lc( $pod )
-         && (!$self->{ rooms }[$room] || $self->{ rooms }[$room] eq $pod)) {
+         && (!$self->{ rooms }[$room] || $self->{ rooms }[$room] =~ /^$pod+$/)) {
            my $next = Storable::dclone( $self );
-           $next->{ energy } += $out + (2 - length( $self->{ rooms }[$room] )) * $energy{ $pod };
+           $next->{ energy } += $out + ($self->{ spots } - length( $self->{ rooms }[$room] )) * $energy{ $pod };
            $self->{ rooms }[$idx] .= $pod;
            $next->{ rooms }[$room] .= $pod;
-           return ( $next );
+           return( $next );
         }
        else {
          $left--;
@@ -96,8 +95,7 @@ my %energy = (
      $left--;
     }
 
-   $out = $energy{ $pod };
-   $out += $energy{ $pod } unless ($self->{ rooms }[$idx]);
+   $out = ($self->{ spots } - length( $self->{ rooms }[$idx] )) * $energy{ $pod };
    my $right = 2 * ($idx + 1) + 1;
    while ($right <= $self->{ right }{ $pod }) {
      $out += $energy{ $pod };
@@ -109,12 +107,12 @@ my %energy = (
        # Check if we can move in
        my $room = ord($self->{ hall }[$right]) - 97;
        if ($self->{ hall }[$right] eq lc( $pod )
-         && (!$self->{ rooms }[$room] || $self->{ rooms }[$room] eq $pod)) {
+         && (!$self->{ rooms }[$room] || $self->{ rooms }[$room] =~ /^$pod+$/)) {
            my $next = Storable::dclone( $self );
-           $next->{ energy } += $out + (2 - length( $self->{ rooms }[$room] )) * $energy{ $pod };
+           $next->{ energy } += $out + ($self->{ spots } - length( $self->{ rooms }[$room] )) * $energy{ $pod };
            $self->{ rooms }[$idx] .= $pod;
            $next->{ rooms }[$room] .= $pod;
-           return ( $next );
+           return( $next );
         }
        else {
          $right++;
@@ -155,11 +153,13 @@ my %energy = (
     hall => [ '', '', 'a', '', 'b', '', 'c', '', 'd', '', '' ],
     left => { A => 0, B => 0, C => 0, D => 0 },
     right => { A => 10, B => 10, C => 10, D => 10 },
+    spots => 0,
   };
 
   for my $line (Path::Tiny::path( $input_file )->lines_utf8( { chomp => 1 } )) {
     $line =~ tr/ABCD//cd;
     next unless $line;
+    $self->{ spots }++;
     for my $i (0 .. length( $line ) - 1) {
       $self->{ rooms }[$i] = substr( $line, $i, 1 ) . $self->{ rooms }[$i];
      }
@@ -172,7 +172,10 @@ my %energy = (
 
 my $input_file = $ARGV[0] || 'input23.txt';
 my $amp = Amphipods->new( $input_file );
-
+my $match = 'A' x $amp->{ spots }
+          . 'B' x $amp->{ spots }
+          . 'C' x $amp->{ spots }
+          . 'D' x $amp->{ spots };
 my $min_energy = -1;
 my @moves = ( $amp );
 my %found;
@@ -184,7 +187,7 @@ while (@moves) {
    next if ($found{ $key } && $found{ $key }->{ energy } <= $move->{ energy });
    $found{ $key } = $move;
 
-   if ($move->rooms eq 'AABBCCDD') {
+   if ($move->rooms eq $match) {
      $min_energy = $move->{ energy } if ($min_energy < 0 || $min_energy > $move->{ energy });
     }
 
